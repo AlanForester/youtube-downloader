a89b1954b94d41d1cabbf88457876dbc22a4938b
Alan Forester <alan@forester.pro>
2025-04-28 14:44:36 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module youtube-downloader
+
+go 1.23.3
+
+require (
+	github.com/bitly/go-simplejson v0.5.1 // indirect
+	github.com/dlclark/regexp2 v1.11.5 // indirect
+	github.com/dop251/goja v0.0.0-20250309171923-bcd7cc6bf64c // indirect
+	github.com/go-sourcemap/sourcemap v2.1.4+incompatible // indirect
+	github.com/google/pprof v0.0.0-20250423184734-337e5dd93bb4 // indirect
+	github.com/kkdai/youtube/v2 v2.10.3 // indirect
+	github.com/yt-dlp/yt-dlp v0.0.0-20250427001634-8cb08028f5be // indirect
+	golang.org/x/text v0.24.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func main() {
+	videoURL := "https://www.youtube.com/watch?v=kzcgel0ddHI"
+	outputFile := "output_video.mp4"
+
+	// Проверяем, установлен ли yt-dlp
+	_, err := exec.LookPath("yt-dlp")
+	if err != nil {
+		fmt.Println("yt-dlp не установлен. Установите его с помощью команды:")
+		fmt.Println("brew install yt-dlp")
+		os.Exit(1)
+	}
+
+	fmt.Println("Начинаем загрузку видео...")
+
+	// Формируем команду для скачивания
+	cmd := exec.Command("yt-dlp",
+		"-f", "best[ext=mp4]", // Выбираем лучший формат mp4
+		"-o", outputFile, // Имя выходного файла
+		"--no-playlist", // Не скачивать плейлист
+		"--no-warnings", // Отключаем предупреждения
+		videoURL,
+	)
+
+	// Перенаправляем вывод команды в stdout и stderr
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// Запускаем команду
+	err = cmd.Run()
+	if err != nil {
+		fmt.Printf("Ошибка при скачивании видео: %v\n", err)
+		os.Exit(1)
+	}
+
+	absPath, _ := filepath.Abs(outputFile)
+	fmt.Printf("Видео успешно загружено: %s\n", absPath)
+}
